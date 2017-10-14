83a64ef40f41ed3923eb20b29c1ac376b99dd42d
Philipp Pa <RussDragon9000@gmail.com>
2017-10-14 22:10:18 +0300

    WIP on grid class and draw-prototype

--- grid.lua
+++ grid.lua
@@ -1,9 +1,63 @@
-local class = {}
+--[[
+		Graph version of grid for mazes. On the init state obj.grid has some unconnected nodes
+		which will be available for maze algorithms.
+]]
+
+local grid = {}
 local aux = {}
 
-class.__index = class
+grid.__index = grid
 
-class.new = function(rows, columns, is_wall_opened) 
-	if not rows or not columns then error("Can not create grid. Rows and columns must be specified.") end
-	
+function grid:new(rows, columns, walls_mode) 
+	-- walls_mode – is walls enabled
+
+	if not rows or not columns then error('Can not create grid. Rows and columns must be specified.') end
+	if rows <= 1 or columns <= 1 then 
+		error('Rows and columns must be greater than 1. Minimal size of grid is 2x2.') 
+	end
+
+	local walls_mode = not not walls_mode
+
+	local obj = {}
+
+	obj.rows = rows
+	obj.columns = columns
+	obj.size = rows * columns
+
+	obj.grid = {}
+
+	for y = 1, columns do
+		obj.grid[y] = {}
+
+		for x = 1, rows do
+			obj.grid[y][x] = {
+				right_wall = walls_mode,
+				bottom_wall = walls_mode,
+
+				__additions = {}
+			}
+		end
+	end
+
+	return setmetatable( obj, self )
 end
+
+function grid:get(coords)
+	return self[y] and self[y][x]
+end
+
+function grid:open_walls()
+	for _, v in pairs(self.grid) do
+		v.right_wall = false
+		v.bottom_wall = false
+	end
+end
+
+function grid:close_walls()
+	for _, v in pairs(self.grid) do
+		v.right_wall = false
+		v.bottom_wall = false
+	end
+end
+
+return grid
--- main.lua
+++ main.lua
@@ -1,7 +1,41 @@
 -- Main controller of graphics and input
 
-function love.load()
+local grid_class = require'grid'
 
+local WINDOW_W, WINDOW_H = 800, 800
+local ROWS, COLUMNS = 80, 50
+
+local draw_conf = {
+	grid = {}
+}
+
+local function hex_to_rgb(str)
+
+end
+
+local function setup_grid_draw(conf)
+	local conf = conf or {}
+
+	draw_conf.grid.h_length = conf.h_length or WINDOW_H / ROWS or error('Horizontal length must be specified.')
+	draw_conf.grid.v_length = conf.v_length or WINDOW_W / COLUMNS or error('Vertical length must be specified.')
+
+	draw_conf.grid.thickness = conf.thickness or 1
+	
+	draw_conf.grid.color = conf.color or '#000000'
+	draw_conf.grid.highlight_color = conf.highlight_color or '#FF0000'
+	draw_conf.grid.back_color = conf.back_color or '#FFFFFF'
+end
+
+function love.load()
+	love.graphics.setBackgroundColor(255, 255, 255, 255)
+	love.window.setMode(WINDOW_W, WINDOW_H, windowed, false, 0)
+
+  title = tostring(window_w) .. " x " .. tostring(window_h)
+  love.window.setTitle(title)
+
+  setup_grid_draw()
+
+  local grid = grid_class:new(ROWS, COLUMNS)
 end
 
 function love.update(dt)
@@ -9,5 +43,5 @@ function love.update(dt)
 end
 
 function love.draw()
-
+	
 end
