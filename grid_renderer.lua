816ee1ad29d67abf20fce050124783e60f78aa6a
Philipp Palutin <russdragon9000@gmail.com>
2017-11-16 23:04:33 +0300

    Added draw config and refactored old render constructor.

--- grid_renderer.lua
+++ grid_renderer.lua
@@ -1,4 +1,12 @@
--- WARNING: DO NOT USE THIS CODE IN PRODUCTION. THIS IS TEST ONLY.
+local type, tonumber, error, setmetatable = type, tonumber, error, setmetatable
+
+local DRAW_DEFAULTS = {
+	thickness = 1,
+	wall_color = "#000000",
+	back_color = "#FFFFFF",
+	highlight_color = "#FF0000"
+}
+
 local renderer = {}
 
 renderer.__index = renderer
@@ -21,59 +29,63 @@ local function hex2rgb(hex)
 				 #hex > 7 and tonumber(hex:sub(7,8) or 255)
 end
 
-function renderer:new(grid_obj, ox, oy, window_w, window_h)
+function renderer:new(grid_obj, draw_mask)
 	if not grid_obj then error('Grid object must be passed to render.') end
+	local draw_mask = draw_mask or {}
 
+	local wh, ww = love.window.getMode()
 	local obj = {}
+	obj._draw_conf = {
+		ox = draw_mask.ox or 0,
+		oy = draw_mask.oy or 0,
+		x = draw_mask.x or ww,
+		y = draw_mask.y or wh,
 
-	obj.draw_conf = {	-- !!!!!!!!!!!!
-	window_w = false,
-	window_h = false,
-	v_length = false,
-	h_length = false,
-	ox = false,
-	oy = false,
-	thickness = 1,
-	color = '#000000',
-	highlight_color = '#FF0000',
-	back_color = '#FFFFFF' 
+		rows = grid_obj:get_height(),
+		columns = grid_obj:get_width(),
+		
+		thickness = DRAW_DEFAULTS.thickness,
+		wall_color = DRAW_DEFAULTS.wall_color,
+		back_color = DRAW_DEFAULTS.back_color,
+		highlight_color = DRAW_DEFAULTS.highlight_color,
 	}
 
-	obj.grid = grid_obj
-
-	obj.draw_conf.window_h = window_h
-	obj.draw_conf.window_w = window_w
-
-	obj.draw_conf.v_length = window_h / grid_obj.rows
-	obj.draw_conf.h_length = window_h / grid_obj.columns
-
-	obj.draw_conf.ox = ox
-	obj.draw_conf.oy = oy
-	
+	obj._grid = grid_obj
 	return setmetatable(obj, self)
 end
 
-function renderer:setup_draw(conf)
+function renderer:setup_draw(draw_mask, thickness, wall_color, highlight_color, back_color)
+	if draw_mask and type(draw_mask) then 
+		self._draw_conf.ox = draw_mask.ox
+		self._draw_conf.oy = draw_mask.oy
+		self._draw_conf.x = draw_mask.x
+		self._draw_conf.y = draw_mask.y
+	end
+
+	self._draw_conf.thickness = thickness or self._draw_conf.thickness 
+	self._draw_conf.wall_color = wall_color or self._draw_conf.wall_color
+	self._draw_conf.back_color = back_color or self._draw_conf.back_color
+	self._draw_conf.highlight_color = highlight_color or self._draw_conf.highlight_color
 end
 
 function renderer:update(dt)
 end
 
 function renderer:draw()
-	local ox, oy = self.draw_conf.ox, self.draw_conf.oy
-	local w, h = self.draw_conf.h_length, self.draw_conf.v_length
+	local w, h = self._draw_conf.x/self._draw_conf.columns, self._draw_conf.y/self._draw_conf.rows
+	local ox, oy = self._draw_conf.ox, self._draw_conf.oy
 
-	for x, y in self.grid:iter() do
-			for k, v in pairs(vertfromwalls(ox+(w*x), oy+(h*y), w, h, self.grid:getrb(x, y))) do
-				local r, g, b = 0, 0, 0
-	      love.graphics.setColor(r, g, b)
-	      love.graphics.setLineWidth(1)
-	      love.graphics.line(v)
+	for x, y in self._grid:iter() do
+		for _, v in pairs(vertfromwalls(ox+(w*x), oy+(h*y), w, h, self._grid:getrb(x, y))) do
+			local r, g, b = 0, 0, 0
+      love.graphics.setColor(r, g, b)
+      love.graphics.setLineWidth(1)
+      love.graphics.line(v)
 		end
 	end
 
-  love.graphics.line(ox, oy+1, self.draw_conf.window_w, oy+1) -- Borders on the top and left
-  love.graphics.line(ox+1, oy, ox+1, self.draw_conf.window_h)
+  love.graphics.line(ox, oy+1, self._draw_conf.max_x, oy+1) -- Borders on the top and left
+  love.graphics.line(ox+1, oy, ox+1, self._draw_conf.max_y)
 end
 
 return renderer
